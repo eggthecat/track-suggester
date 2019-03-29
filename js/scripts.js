b326d9c35157ddbb2512b4c8a3638f171d2d2571
Dominic Montelongo <montelnogodominic@gmail.com>
2019-03-29 13:14:49 -0700

    Add results class to HTML and coresponding material to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-$(document).ready(function() {
-  $("#form").submit(function(event) {
-    event.preventDefault();
-    var animal = $('input[name=animal]:checked').val();
-
-    if(animal === "Seals") {
-      $("#Eagles").hide();
-      $("#Monkeys").hide();
-      $("#infoSeals").show();
-    } else if(animal === "Eagles") {
-      $("#Monkeys").hide();
-      $("#infoSeals").hide();
-      $("#Eagles").show();
-    } else if(animal === "Monkeys") {
-      $("#Eagles").hide();
-      $("#infoSeals").hide();
-      $("#Monkeys").show();
-    } else {
-
-    };
-
-  });
-  });
+// $(document).ready(function() {
+//   $("#form").submit(function(event) {
+//     event.preventDefault();
+//     var animal = $('input[name=animal]:checked').val();
+//
+//     if(animal === "Seals") {
+//       $("#Eagles").hide();
+//       $("#Monkeys").hide();
+//       $("#infoSeals").show();
+//     } else if(animal === "Eagles") {
+//       $("#Monkeys").hide();
+//       $("#infoSeals").hide();
+//       $("#Eagles").show();
+//     } else if(animal === "Monkeys") {
+//       $("#Eagles").hide();
+//       $("#infoSeals").hide();
+//       $("#Monkeys").show();
+//     } else {
+//
+//     };
+//
+//   });
+//   });
