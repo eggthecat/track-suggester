343a38fce57ab6284428daece71013cc93093521
Dominic Montelongo <montelnogodominic@gmail.com>
2019-03-29 16:52:01 -0700

    Modify html and js element

--- js/scripts.js
+++ js/scripts.js
@@ -38,28 +38,3 @@ $(document).ready(function() {
     };
 
   });
-  });
-
-  $(document).ready(function() {
-  $("#form").submit(function(event) {
-    event.preventDefault();
-    var animal = $('input[name=animal]:checked').val();
-
-    if(animal === "Seals") {
-      $("#Eagles").hide();
-      $("#Monkeys").hide();
-      $("#infoSeals").show();
-    } else if(animal === "Eagles") {
-      $("#Monkeys").hide();
-      $("#infoSeals").hide();
-      $("#Eagles").show();
-    } else if(animal === "Monkeys") {
-      $("#Eagles").hide();
-      $("#infoSeals").hide();
-      $("#Monkeys").show();
-    } else {
-
-    };
-
-  });
-  });
