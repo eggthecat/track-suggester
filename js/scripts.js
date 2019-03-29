7b04e677b538d38d0e2839ffc2caac2a45cdbab5
Dominic Montelongo <montelnogodominic@gmail.com>
2019-03-29 14:13:14 -0700

    Add options to js and modify html a bit

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,33 @@
 //
 //   });
 //   });
+
+$(function() {
+
+  $("form#politics").submit(function(event) {
+    event.preventDefault();
+    var qOne = parseInt($("#qOne").val());
+    var qTwo = parseInt($("#qTwo").val());
+    var qThree = parseInt($("#qThree").val());
+
+    if(qOne === 1 && qTwo === 1 && qThree === 1) {
+      $("#Liberal").show();
+      $("#Conservative").hide();
+      $("#Moderate").hide();
+    } else if (qOne === 2 && qTwo === 2 && qThree === 2) {
+      $("#Conservative").show();
+      $("#Liberal").hide();
+      $("#Moderate").hide();
+    } else {
+      $("#Moderate").show();
+      $("#Liberal").hide();
+      $("#Conservative").hide();
+
+    };
+
+  });
+
+
+
+
+});
