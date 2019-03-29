9837923f40c0f9ca2fc9c3cbd1a769ceb73a7197
Dominic Montelongo <montelnogodominic@gmail.com>
2019-03-29 15:50:44 -0700

    change js and html layout to alter ui presentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,117 @@
-// $(document).ready(function() {
-//   $("#form").submit(function(event) {
-//     event.preventDefault();
-//     var animal = $('input[name=animal]:checked').val();
-//
-//     if(animal === "Seals") {
-//       $("#Eagles").hide();
-//       $("#Monkeys").hide();
-//       $("#infoSeals").show();
-//     } else if(animal === "Eagles") {
-//       $("#Monkeys").hide();
-//       $("#infoSeals").hide();
-//       $("#Eagles").show();
-//     } else if(animal === "Monkeys") {
-//       $("#Eagles").hide();
-//       $("#infoSeals").hide();
-//       $("#Monkeys").show();
-//     } else {
-//
-//     };
-//
-//   });
-//   });
-
-$(function() {
-
-  $("form#programs").submit(function(event) {
+$(document).ready(function() {
+  $("#form").submit(function(event) {
     event.preventDefault();
-    var qOne = parseInt($("#qOne").val());
-    var qTwo = parseInt($("#qTwo").val());
-    var qThree = parseInt($("#qThree").val());
-    var qFour = parseInt($("#qFour").val());
-    var qFive = parseInt($("#qFive").val());
+    var coder = $('input[name=animal]:checked').val();
 
-    if(qOne ===1) {
-      $("#Scratch").show();
-      $("#Swift").hide();
-      $("#Cscharp").hide();
+    if(coder === "Young") {
+      $("#Error").hide();
+      $("#Apple").hide();
+      $("#Young").show();
+      $("#Micro").hide();
       $("#Android").hide();
-    } else if (qTwo === 1 || qTwo === 2) {
-      $("#Scratch").show();
-      $("#Swift").hide();
-      $("#Cscharp").hide();
+    } else if(coder === "Error") {
+      $("#Apple").hide();
+      $("#Young").hide();
+      $("#Error").show();
+      $("#Micro").hide();
       $("#Android").hide();
-    } else if(qThree ===1) {
-      $("#Scratch").hide();
-      $("#Swift").show();
-      $("#Cscharp").hide();
+    } else if(coder === "Apple") {
+      $("#Error").hide();
+      $("#Young").hide();
+      $("#Apple").show();
+      $("#Mirco").hide();
       $("#Android").hide();
-    } else if(qFour ===1) {
-      $("#Scratch").hide();
-      $("#Swift").hide();
-      $("#Cscharp").show();
+    } else if(coder === "Micro") {
+      $("#Error").hide();
+      $("#Young").hide();
+      $("#Apple").show();
+      $("#Micro").show();
       $("#Android").hide();
-    } else(qFive ===1) {
-      $("#Scratch").hide();
-      $("#Swift").hide();
-      $("#Cscharp").hide();
-      $("#Android").show();
+    } else (coder === "Android") {
+      $("#Error").hide();
+      $("#Young").hide();
+      $("#Apple").show();
+      $("#Micro").hide();
+      $("#Android").hide();
+    }
+
     };
+
   });
+  });
+
+  // <form id="coderForm">
+  //       <div class="buttons">
+  //         <div class="radio">
+  //           <label>
+  //             <input type="radio" name="coder" value="Young" > I'm under 16
+  //           </label>
+  //         </div>
+  //         <div class="radio">
+  //           <label>
+  //             <input type="radio" name="coder" value="Error"> I beleive the earth is flat
+  //           </label>
+  //         </div>
+  //         <div class="radio">
+  //           <label>
+  //             <input type="radio" name="coder" value="Apple"> Apple is superior
+  //           </label>
+  //         </div>
+  //         <div class="radio">
+  //          <label>
+  //            <input type="radio" name="coder" value="Micro" > Born and raised on Microsoft
+  //          </label>
+  //        </div>
+  //        <div class="radio">
+  //          <label>
+  //            <input type="radio" name="coder" value="Android"> Android is King
+  //          </label>
+  //        </div>
+  //           <br>
+  //           <hr>
+  //           <button type="submit" name="button">Explore</button>
+  //           <!-- <p><input type="button" id="btncode" value="Präsentiere Die Antwort"></p> -->
+  //         <hr>
+  //         </div>
+  //       </div>
+  //     </form>
+// $(function() {
+//
+//   $("form#programs").submit(function(event) {
+//     event.preventDefault();
+//     var qOne = parseInt($("#qOne").val());
+//     var qTwo = parseInt($("#qTwo").val());
+//     var qThree = parseInt($("#qThree").val());
+//     var qFour = parseInt($("#qFour").val());
+//     var qFive = parseInt($("#qFive").val());
+//
+//     if(qOne ===1) {
+//       $("#Scratch").show();
+//       $("#Swift").hide();
+//       $("#Cscharp").hide();
+//       $("#Android").hide();
+//     } else if (qTwo === 1 || qTwo === 2) {
+//       $("#Scratch").show();
+//       $("#Swift").hide();
+//       $("#Cscharp").hide();
+//       $("#Android").hide();
+//     } else if(qThree ===1) {
+//       $("#Scratch").hide();
+//       $("#Swift").show();
+//       $("#Cscharp").hide();
+//       $("#Android").hide();
+//     } else if(qFour ===1) {
+//       $("#Scratch").hide();
+//       $("#Swift").hide();
+//       $("#Cscharp").show();
+//       $("#Android").hide();
+//     } else(qFive ===1) {
+//       $("#Scratch").hide();
+//       $("#Swift").hide();
+//       $("#Cscharp").hide();
+//       $("#Android").show();
+//     };
+//   });
 
   //   if(qOne === 1 && qTwo === 1 && qThree === 1) {
   //     $("#Liberal").show();
